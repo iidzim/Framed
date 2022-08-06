56f5cd49691500077cf0bf073fc65dd43cdc3363
iidzim9 <ikramid99@gmail.com>
2022-08-06 11:19:16 +0100

    “

---
+++ backend/push.sh
@@ -0,0 +1,9 @@
+!/bin/bash
+echo “running command from” $PWD
+cd $PWD
+rm -rf node_modules/ dist/
+git checkout backend
+git add .
+echo “Enter commit message: “
+git commit -am “$commitMessage”
+git push
