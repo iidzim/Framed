60e6392f7952898661d53cd34bc0ec9a97a00fd8
iidzim9 <ikramid99@gmail.com>
2022-08-06 11:01:17 +0100

    2nd commit

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -11,6 +11,7 @@ bootstrap();
 //td: script for git push
 //td: git branching and merging
 	//? https://www.youtube.com/watch?v=Q1kHG842HoI&list=LL&index=14&t=2259s&ab_channel=SuperSimpleDev
+		//= second commit to branch
 
 //+ add middleware & AuthGuard to protect this endpoint
 //+ create jwt for logged in user
